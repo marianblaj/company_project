f1fa1b95f81529e61a3e4a0c5d19e42b638325de
Marian <marian.blaj@yahoo.com>
2020-02-11 15:33:51 +0200

    start proiect

---
+++ src/test/java/com/example/company_employee/CompanyEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.company_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
